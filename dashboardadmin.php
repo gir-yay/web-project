2d8f2d6c6bcc9f217e97c5d11c3bd34bff26a388
Mohammed EZ-ZOUAK <120412811+MedEZZOUAK@users.noreply.github.com>
2023-04-12 19:20:19 -0400

    added a delete button for the admin dashbors

--- dashboardadmin.php
+++ dashboardadmin.php
@@ -19,7 +19,7 @@ $result = mysqli_query($conn, $sql);
 
 //print the result in a table format
 echo "<table>";
-echo "<tr><th>ID</th><th>Last Name</th><th>First Name</th><th>Email</th><th>Age</th></tr>";
+echo "<tr><th>ID</th><th>Last Name</th><th>First Name</th><th>Email</th><th>Age</th><th>Action</th></tr>";
 foreach ($result as $row) {
     echo "<tr>";
     echo "<td>" . $row['id'] . "</td>";
@@ -27,29 +27,66 @@ foreach ($result as $row) {
     echo "<td>".$row['firstname']."</td>";
     echo "<td>".$row['email']."</td>";
     echo "<td>".$row['age']."</td>";
+    echo "<td>";
+    echo "<form method='post'>";
+    echo "<input type='hidden' name='id' value='".$row['id']."'>";
+    echo "<input type='submit' name='delete_influencer' value='Delete'>";
+    echo "</form>";
+    echo "</td>";
     echo "</tr>";
 }
+
 echo "</table>";
 
-// show all the brand from the database as a table
+// check if the delete button for influencer has been clicked
+if(isset($_POST['delete_influencer'])){
+    $id = $_POST['id'];
+    $sql = "DELETE FROM influencer WHERE id = '$id'";
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        echo "Record deleted successfully.";
+    } else {
+        echo "Error deleting record: " . mysqli_error($conn);
+    }
+}
+
 echo "<h1>BRAND</h1>";
 $sql = "SELECT * FROM entreprise";
 $result = mysqli_query($conn, $sql);
-//print the result in a table format
+
 echo "<table>";
-echo "<tr><th>ID</th><th>Name</th><th>Email</th><th>CA</th></tr>";
+echo "<tr><th>ID</th><th>Name</th><th>Email</th><th>CA</th><th>Action</th></tr>";
 foreach ($result as $row) {
     echo "<tr>";
     echo "<td>" . $row['id'] . "</td>";
     echo "<td>".$row['Name']."</td>";
     echo "<td>".$row['email']."</td>";
     echo "<td>".$row['CA']."</td>";
+    echo "<td>";
+    echo "<form method='post'>";
+    echo "<input type='hidden' name='id' value='".$row['id']."'>";
+    echo "<input type='submit' name='delete_brand' value='Delete'>";
+    echo "</form>";
+    echo "</td>";
     echo "</tr>";
 }
 echo "</table>";
+
+// check if the delete button for brand has been clicked
+if(isset($_POST['delete_brand'])){
+    $id = $_POST['id'];
+    $sql = "DELETE FROM entreprise WHERE id = '$id'";
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        echo "Record deleted successfully.";
+    } else {
+        echo "Error deleting record: " . mysqli_error($conn);
+    }
+}
+
 ?>
 <style>
-/* Style for the header tags */
+  /* Style for the header tags */
 h1 {
   font-size: 3rem;
   font-weight: bold;
@@ -117,5 +154,26 @@ button {
 button:hover {
   background-color: #AD2E2E;
 }
+
+/* Style for the delete buttons */
+input[type="submit"][name="delete_influencer"], input[type="submit"][name="delete_brand"] {
+  background-color: #FF0000;
+  color: #FFFFFF;
+  border: none;
+  padding: 8px 16px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+input[type="submit"][name="delete_influencer"]:hover, input[type="submit"][name="delete_brand"]:hover {
+  background-color: #AD2E2E;
+}
+
+
+
+
 
 </style>
