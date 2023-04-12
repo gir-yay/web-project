c260d24090896946ee6ee3fa620a965f17bfcc3a
Mohammed EZ-ZOUAK <120412811+MedEZZOUAK@users.noreply.github.com>
2023-04-12 18:38:03 -0400

    Add files via upload

--- dashboardadmin.php
+++ dashboardadmin.php
@@ -7,38 +7,47 @@ session_start();
 if (isset($_SESSION['username'])) {
     // if the session variable is set, redirect to dashboard.php
     $name=$_SESSION['username'];
-    echo "<h1>WELCOME $name</h1>";
+    echo "<h3>WELCOME $name</h3>";
 } else {
     // if the session variable is not set, redirect to admin.php
     header('Location: admin.php');
 }
-//show all the influencer from the database
+// show all the influencer from the database as a table
 echo "<h1>INFLUENCER</h1>";
 $sql = "SELECT * FROM influencer";
 $result = mysqli_query($conn, $sql);  
-//print the result
+
+//print the result in a table format
+echo "<table>";
+echo "<tr><th>ID</th><th>Last Name</th><th>First Name</th><th>Email</th><th>Age</th></tr>";
 foreach ($result as $row) {
-    echo "<div class='influencer'>";
-    echo "<h2>" . $row['id'] . "</h2>";
-    echo "<h2>".$row['lastname']."</h2>";
-    echo "<h2>".$row['firstname']."</h2>";
-    echo "<h2>".$row['email']."</h2>";
-    echo "<h2>".$row['age']."</h2>";
-    echo "</div>";
+    echo "<tr>";
+    echo "<td>" . $row['id'] . "</td>";
+    echo "<td>".$row['lastname']."</td>";
+    echo "<td>".$row['firstname']."</td>";
+    echo "<td>".$row['email']."</td>";
+    echo "<td>".$row['age']."</td>";
+    echo "</tr>";
 }
-//show all the brand from the database
+echo "</table>";
+
+// show all the brand from the database as a table
 echo "<h1>BRAND</h1>";
 $sql = "SELECT * FROM entreprise";
 $result = mysqli_query($conn, $sql);
-//print the result
+//print the result in a table format
+echo "<table>";
+echo "<tr><th>ID</th><th>Name</th><th>Email</th><th>CA</th></tr>";
 foreach ($result as $row) {
-    echo "<div class='brand'>";
-    echo "<h2>" . $row['id'] . "</h2>";
-    echo "<h2>".$row['Name']."</h2>";
-    echo "<h2>".$row['email']."</h2>";
-    echo "<h2>".$row['CA']."</h2>";
-    echo "</div>";
+    echo "<tr>";
+    echo "<td>" . $row['id'] . "</td>";
+    echo "<td>".$row['Name']."</td>";
+    echo "<td>".$row['email']."</td>";
+    echo "<td>".$row['CA']."</td>";
+    echo "</tr>";
 }
+echo "</table>";
+
 
 
 
