bc36b66ab19d97cbd8fc0b18c4ec6e3c83115aff
SalmaZ29 <s.zaoioui@gmail.com>
2023-04-14 01:02:03 +0000

    home

--- index.php
+++ index.php
@@ -5,16 +5,32 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>My Homepage</title>
-	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-	<div class="container">
-		<h1>Welcome to My Website</h1>
-		<p>Please select one of the following options:</p>
-		<ul>
-			<li><a href="LOGIN.php">Login</a></li>
-			<li><a href="signup.php">Signup</a></li>
-		</ul>
+	<div class="home"> 
+	<nav>
+			<img src="logo1.png" class="logo">
+			<ul>
+				<li> <a href="#"> Home</a></li>
+				<li> <a href="#"> About as</a></li>
+				<li> <a href="#"> Services</a></li>
+				<li> <a href="#"> Contact</a></li>
+			</ul>
+			<div>
+			    <a href="LOGIN.php" class="btn">Login</a>
+		</div>
+		</nav>
+		<div class="content">
+			<h1 class="anim"> Collab for <br> Influencer & Brand</h1>
+			<p class="anim"> Pour réussir votre campagne de marketing
+				 d'influence, vous devez travailler avec des
+				  influenceurs expérimentés, qui partagent 
+				  les valeurs de votre marque. </p>
+			<a  href="signup.php" class ="btn anim">Join Now</a>
+		</div>
+		<img src="photoPRP.png" class="features-img anim">
 	</div>
+		
 </body>
 </html>
