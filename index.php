a133f8c8bd4b8362bccb01e2ec965cffa11c1a85
SalmaZ29 <s.zaoioui@gmail.com>
2023-04-14 11:11:13 +0000

    contactUs

--- index.php
+++ index.php
@@ -5,17 +5,18 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>My Homepage</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
 	<link rel="stylesheet" href="style.css">
 </head>
-<body>
-	<div class="home"> 
+<body> <section><div class="home"> 
 	<nav>
 			<img src="logo1.png" class="logo">
 			<ul>
 				<li> <a href="#"> Home</a></li>
 				<li> <a href="#"> About as</a></li>
 				<li> <a href="#"> Services</a></li>
-				<li> <a href="#"> Contact</a></li>
+				<li> <a href="#contacte"> Contact</a></li>
 			</ul>
 			<div>
 			    <a href="LOGIN.php" class="btn">Login</a>
@@ -30,7 +31,27 @@
 			<a  href="signup.php" class ="btn anim">Join Now</a>
 		</div>
 		<img src="photoPRP.png" class="features-img anim">
-	</div>
+	</div> </section>
+	
 		
+    <footer>
+		<div> <h2 id="contacte" > Contacter nous</h2></div>
+<form >
+    <input type="text" placeholder="Nom">
+    <input type="email" placeholder="E-mail" >
+    <textarea placeholder=" Votre message ici"></textarea>
+    <button> Envoyer</button>
+</form>
+<div id="trait"> </div>
+<div id="copyrighticon"> 
+    <div id="copyright"> <span> ©2023 </span></div>
+    <div id="icons">
+        <a href="http://www.twitter.fr"> <i class="fa fa-twitter">  </i></a>
+        <a href="http://www.facebook.fr"> <i class="fa fa-facebook">  </i></a>
+        <a href="http://www.instagram.fr"> <i class="fa fa-instagram">  </i></a>
+    </div>
+</div>
+
+    </footer>
 </body>
 </html>
