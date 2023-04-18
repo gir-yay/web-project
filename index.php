02b0cc64920c6e13e5469be2765712c6bf309746
SalmaZ29 <s.zaoioui@gmail.com>
2023-04-18 13:59:21 +0000

    about us

--- index.php
+++ index.php
@@ -28,15 +28,54 @@
 				 d'influence, vous devez travailler avec des
 				  influenceurs expérimentés, qui partagent 
 				  les valeurs de votre marque. </p>
-			<a  href="signup.php" class ="btn anim">Join Now</a>
+			<a  href="signup.php" class ="btn anim">Join Now</a> 
+			
+			<span class="image">   </span>
 		</div>
-		<img src="photoPRP.png" class="features-img anim">
-	</div> </section>
-	
+		<img src="photo1.png" class="features-img anim">
+
 		
-    <footer>
+	</div> </section>
+	<div class="about"> 
+<h2> About us</h2>
+<p> La plateforme spécialisée en marketing d'influence
+Collab simplifie les collaborations entre marques et créateurs de contenu</p>
+	</div>
+	<div class="why">
+             <h1> Notre engagement</h1>
+             <p> Nous facilitons la création de partenariats qui bénéficient aux marques et aux influenceurs</p>
+	</div>
+	<div class="avis1">
+		<span class="avis">
+        <img src="quotes.png" alt="">
+		<h2> Fati</h2>
+		<p> Dovozo est un formidable outil, simple d'utilisation qui facilite les contacts entre influenceurs et marques. 
+			Pour les entreprises comme la nôtre cela nous fait gagner un temps précieux dans la recherche de collaborations. 
+			De plus, l'offre est particulièrement qualitative. Quel que soit votre budget, vous trouverez des influenceurs de
+			toutes audiences et évoluant dans tous types de domaines, mais toujours très professionnels. La communication est 
+			fluide et les échanges rapides. Je recommande donc à toute marque souhaitant développer son marketing d'influence.</p>
+</span>
+		<span class="avis">
+        <img src="quotes.png" alt="">
+		<h2> Riyad</h2>
+		<p> Dovozo est un excellent outil pour entrer rapidement en relation avec de nombreux influenceurs qualifiés.
+			 Dans le cadre du développement de Mon Petit Coin Vert nous avons pu faire découvrir notre offre à plus de
+			  50 influenceurs Instagram et Youtube en quelques semaines seulement. L'outil nous permet également d'améliorer
+			   notre référencement naturel en ciblant des blogs et sites de contenus de qualités.</p>
+</span>
+		
+		<span class="avis">
+        <img src="quotes.png" alt="">
+		<h2> abdo</h2>
+		<p> La plateforme Dovozo a grandement facilité le quotidien de l'équipe en charge des partenariats sur
+			Commentseruiner.com. Il devient très simple de contacter et créer de nouveaux partenariat avec des influenceurs. 
+			De plus, l'outil permet de suivre les objectifs et incite les deux parties à clôturer le partenariat en bonne entente.
+			 En quelques mots : gain de temps, efficacité et rentabilité.</p>
+</span>
+</div>
+	          <footer>
 		<div> <h2 id="contacte" > Contacter nous</h2></div>
-<form >
+    <form >
     <input type="text" placeholder="Nom">
     <input type="email" placeholder="E-mail" >
     <textarea placeholder=" Votre message ici"></textarea>
@@ -51,7 +90,6 @@
         <a href="http://www.instagram.fr"> <i class="fa fa-instagram">  </i></a>
     </div>
 </div>
-
-    </footer>
+</footer>
 </body>
 </html>
