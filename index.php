31876bbec81968e2df31eb3701345279d5adeb56
SalmaZ29 <s.zaoioui@gmail.com>
2023-04-21 04:54:07 +0000

    edit

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 			<img src="logo1.png" class="logo">
 			<ul>
 				<li> <a href="#"> Home</a></li>
-				<li> <a href="#"> About as</a></li>
+				<li> <a href="#about"> About as</a></li>
 				<li> <a href="#"> Services</a></li>
 				<li> <a href="#contacte"> Contact</a></li>
 			</ul>
@@ -28,23 +28,80 @@
 				 d'influence, vous devez travailler avec des
 				  influenceurs expérimentés, qui partagent 
 				  les valeurs de votre marque. </p>
-			<a  href="signup.php" class ="btn anim">Join Now</a> 
-			
-			<span class="image">   </span>
-		</div>
-		<img src="photo1.png" class="features-img anim">
+			<a  href="signup.php" class ="btn anim">Join Now</a> </div> 
+			<img src="head3.png" width="750px" class="features-img anim"></section>
+				
+	                       <!-- about -->
+			<section id="about"> 
+	    <div class="about-left-col"> 
+	       <img src="header-bg.jpg" ></div>
+	  <div class="about-right-col"> 
+	    <div class="about-text"> 
+           <h1> About us</h1>
+           <p> Collab est une plateforme innovante qui vise à connecter les influenceurs et 
+			   les marques pour faciliter les collaborations. Notre mission est de créer des 
+			   partenariats authentiques et réussis entre les deux parties, en mettant l'accent sur
+			    la pertinence et la qualité des collaborations. Nous croyons que chaque collaboration devrait
+				 être unique et personnalisée pour répondre aux besoins spécifiques de chaque marque et de chaque
+				  influenceur. Notre équipe expérimentée travaille en étroite collaboration avec les marques et les 
+				  influenceurs pour les aider à atteindre leurs objectifs de marketing et à maximiser leur potentiel sur
+				   les réseaux sociaux. Nous sommes fiers d'offrir une expérience fluide, efficace et agréable pour tous nos utilisateurs. 
+				   <br> <br>
+				  <strong>  Rejoignez-nous dès maintenant.<strong\> <br> <br> <br> 
+				</p>
+				<a  href="signup.php" class ="btn anim">Join Now</a>
+      </div>
+      </div>
+</section>
+                     <!-- features -->
+					 <section id="features"> 
+						<div class="feature-row">
+							<div class="feature-col">
+								<img src="feature1.png" alt="">
+								<h4> Partenariat de qualité</h4>
+								<p> petit texte</p>
+								<br> <br> <br><br> <br> <br>
+							</div>
+							<div class="feature-col"> 
+							<img src="feature2.png" alt="">
+							<h4> Gratuit</h4>
+							<p> petit texte</p>
+							<br> <br> <br><br> <br> <br>
+							</div>
+							<div class="feature-col"> 
+							<img src="feature3.png" alt="">
+							<h4> securite</h4>
+							<p> petit texte </p>
+							<br> <br> <br><br> <br> <br>
+							</div>
+						</div>
+                        <div class="feature-btn"> <a href="signup.php" class ="btn anim"> Join Now </a> </div>
+					 </section>
 
-		
-	</div> </section>
-	<div class="about"> 
-<h2> About us</h2>
-<p> La plateforme spécialisée en marketing d'influence
-Collab simplifie les collaborations entre marques et créateurs de contenu</p>
-	</div>
-	<div class="why">
-             <h1> Notre engagement</h1>
+
+            <!-- why -->
+<section id="why"> 
+	<div> <h1> Pourquoi <br> nous choisir ?</h1></div>
+
+	<div class="why1">
              <p> Nous facilitons la création de partenariats qui bénéficient aux marques et aux influenceurs</p>
 	</div>
+	<div class="why-left">
+		<h4> Vous êtes une marque…</h4>
+		<p>Offrez vous une solution personnalisée pour 
+			votre stratégie d'influence grâce à nos outils
+			 innovants de recherche, d'analyse et de lancement 
+			 de campagnes  </p>
+
+	</div>
+	<div class="why-right">
+		<h4>Vous êtes un influenceur…</h4>
+		<p> Monétisez votre audience et gagnez de 
+			l'argent en répondant à des projets soumis
+			 par les top marques présentes sur Natividi</p>
+
+	</div>
+	</section> 
 	<div class="avis1">
 		<span class="avis">
         <img src="quotes.png" alt="">
