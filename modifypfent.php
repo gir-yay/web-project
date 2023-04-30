e7dcd88bf0876b0e90f3e146e1ec1db65fc66da5
MARYEMRISY <130423203+Maryem-Risy@users.noreply.github.com>
2023-04-30 23:50:28 +0100

    Update modifypfent.php

--- modifypfent.php
+++ modifypfent.php
@@ -18,7 +18,7 @@ $logo='Upload/'.$row['logo'];
 <head>
     <title>Modifier profil entreprise</title>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="modifypfent.css">
+    <link rel="stylesheet" href="./css/modifypfent.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <!-- make a forum with all the info as default -->
