be7129e1afa72f3886d76c04723ef3dbad3880ac
ghizlane <ghlaneiz022@gmail.com>
2023-05-01 07:00:36 +0100

    chat style

--- message_inf.php
+++ message_inf.php
@@ -1,8 +1,4 @@
 <?php
-//add a home button
-echo '<a href="influenceur.php">Return</a>';
-//add a logout button
-echo '<a href="logout.php">Logout</a>';
 // Start the session
 session_start();
 if (!isset($_SESSION['id'])) {
@@ -55,17 +51,28 @@ if ($type == 'ent') {
 <head>
     <title>Message Exchange</title>
     <link rel="stylesheet" href="./css/message.css">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
-    <div class="container">
-        <div class="conversation">
-            <!-- Messages will be displayed here -->
-            <?php
+     <?php
             //get the messages from the database
             //show all the messages between the sender and the receiver in the conversation div
                 $sql = "SELECT * FROM messages WHERE (sender = '$sender' AND receiver = '$receiver') OR (sender = '$receiver' AND receiver = '$sender') ORDER BY timestamp ASC";
-                $result = mysqli_query($conn, $sql);
+                $result = mysqli_query($conn, $sql); ?>
+    
+    <section class="wrapper">
+        <header>
+            <a href="influenceur.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+                <div class="details">
+                    <span><?php echo $receiverName ?></span>
+                </div>
+            </header>
+
+        <div class="chat-area">
+            <!-- Messages will be displayed here -->
+           <?php
+                                       echo '<div class="chat-box">';
+
                 if(mysqli_num_rows($result) > 0){
                     while($row = mysqli_fetch_assoc($result)){
                         $message = $row['message'];
@@ -73,38 +80,42 @@ if ($type == 'ent') {
                         //if the sender is the current user
                         if($row['sender'] == $sender){
                             //display the message in the right side of the conversation div
-                            echo '<div class="message-box" style="text-align: right;">';
-                            echo '<div class="sender">'.$senderName.'</div>';
-                            echo '<div class="timestamp">'.$timestamp.'</div>';
-                            echo '<div class="content">'.$message.'</div>';
+                             echo '<div class="chat outgoing">';
+                            echo '<div class="details"><p>'.$message.'<br>'.$timestamp.'</p></div>';
                             echo '</div>';
+
                         }else{
                             //display the message in the left side of the conversation div
-                            echo '<div class="message-box" style="text-align: left;">';
-                            echo '<div class="sender">'.$receiverName.'</div>';
-                            echo '<div class="timestamp">'.$timestamp.'</div>';
-                            echo '<div class="content">'.$message.'</div>';
+                             echo '<div class="chat incoming">';
+                             echo '<div class="details"><p>'.$message.'<br>'.$timestamp.'</p></div>';
                             echo '</div>';
+                           
                         }
                     }
                 }
+                                            echo '</div>';
+
                 ?>
         </div>
 
-        <form class="message-form" method="post" action="">
-            <textarea name="message" placeholder="Type your message here"></textarea>
-            <button type="submit" name="send">Send</button>
+        <form class="typing-area" method="post" action="">
+            <input type="text" name="message" placeholder="Type your message here...">
+            <button type="submit" name="send"><i class="fa-sharp fa-solid fa-paper-plane fa-lg" ></i></button>
         </form>
-    </div>
+            </section>
 </body>
 </html>
+
 <?php 
+/*
 echo "<br>the sender id is ".$sender."<br>";
 echo "the receiver id is ".$receiver."<br>";
 echo "the type is ".$type."<br>";
 echo "the sender name is ".$senderName."<br>";
 echo "the receiver name is ".$receiverName."<br>";
+*/
 ?>
+
 <?php
 //if the send button is clicked
 if(isset($_POST['send'])){
@@ -118,11 +129,12 @@ if(isset($_POST['send'])){
     //if the message is inserted
     if($result){
         //refresh the page
+        
 ?>
-      <script type="text/javascript">window.location="message_inf.php";</script>
+      <script type="text/javascript">window.location="message_ent.php";</script>
 <?php
 
-      }else{
+}else{
         //display an error message
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
