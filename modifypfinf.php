c5f081ffd42dcd3d7a37fb4e2bfcb1c6a2140afc
MARYEMRISY <130423203+Maryem-Risy@users.noreply.github.com>
2023-05-01 05:59:54 +0100

    Update modifypfinf.php

--- modifypfinf.php
+++ modifypfinf.php
@@ -25,7 +25,7 @@ $age = $row['age'];
     <head>
         <title>Modifier profil influenceur</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="modifypfinf.css">
+        <link rel="stylesheet" type="text/css" href="./css/modifypfinf.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     </head>
