12bbe0d7cb208974af2ca3c13feb9932314cf241
ghizlane <ghlaneiz022@gmail.com>
2023-05-01 07:13:51 +0100

    idk

--- influenceur.php
+++ influenceur.php
@@ -4,7 +4,9 @@
     //check if the session is set
     if(!isset($_SESSION['email'])){
         //if not set redirect to the login page
-        header("Location: login.php");
+?>
+      <script type="text/javascript">window.location="loogin.php";</script>
+<?php
         exit();
     }else{
         //if set get the name of the entreprise
@@ -12,6 +14,8 @@
         $name= $_SESSION['firstname'].' ' .$_SESSION['Lastname'];
         $id=$_SESSION['id'];
     }
+    $_SESSION['type'] = "influencer";
+
 ?>
 
 
@@ -22,59 +26,55 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>influenceur </title>
     <link rel="stylesheet" href="./css/influenceur.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-
+    <h1>influenceur</h1>
+    <h2> <?php echo $name; ?> </h2>
+    <h2> <?php echo $email; ?> </h2>
+    <h2> <?php echo $id; ?> </h2>
+    <!-- A Buton to logout -->
     <nav>
-    <span class="icon">
-        <i class="fa fa-user"></i> 
-          </span><hr>
-        <ul> 
-            <li> <?php echo $name; ?></li><br>
-            <li><i class="fa fa-envelope"></i>  <?php echo $email; ?> </li><br>
-            <li>ID: <?php echo $id; ?> </li>
-            <br>
-  </ul><hr>
-  <ul>
-             <!--  lien pour modifier les infomations de l'entreprise -->
-             <li>
-                <a href="modifypfinf.php"><i class="fa fa-pencil-square-o"></i> Modify Profile</a>
-            </li><br>
-                <!-- lien pour voir les messages recus -->
+        <ul>
+            <li>&#9776; MENU </li><br><br>
+            <!-- Bouton du logout pour detruire la session de l'utilisateur "on click log out" -->
             <li>
-                <a href="messagerec.php"><i class="fa fa-envelope"></i> Messages recu</a>
+            <i class="fa fa-sign-out"></i>
+                <input type="submit" value="Logout" onclick="window.location.href='logout.php'">
+            </li><br>
+
+            <!--  lien pour modifier les infomations de l'entreprise -->
+
+            <li><a href="modifypfinf.php"><i class="fa fa-pencil-square-o"></i> Modify Profile</a>
+            </li><br>
+            <!-- send a request to the admin to delete ur accont  -->
+            <li><a href="delete.php"><i class="fa fa-trash"></i> Delete Account</a>
             </li><br>
             <!-- lien pour contactez l'admin -->
             <li>
             <a href="contact.php"><i class="fa fa-envelope"></i> Contact</a>
             </li><br>
-            <!-- envoyer une demande de suppression du compte  -->
+            <!-- lien pour voir les messages recu -->
             <li>
-                <a href="delete.php"><i class="fa fa-trash"></i>Supprimer mon compte</a>
+                <a href="messagerec.php"><i class="fa fa-envelope"></i> Messages recu</a>
             </li><br>
-            <li>
-                <a href="logout.php"><i class="fa fa-sign-out"></i>Logout</a>
-        </li><br>
-        
-    
+            
+
             
         </ul>
-  </nav>
-  <main>
-    </main>
-</body>
-</html>
+
+    </nav>
+
+    
 <div class="main">
 <?php 
-include 'database.php';
+include_once 'database.php';
 // show all the entreprise as a table
 
 $sql = "SELECT * FROM entreprise";
 $result = $conn->query($sql);
-echo "<h2>Entreprises</h2>";
     // output data of each row
     echo "<table border='1'>
     <tr>
@@ -96,10 +96,10 @@ echo "<h2>Entreprises</h2>";
         // add a button to make an suggestion to the influencer
         echo "<button type='button' class='suggestion-btn'>suggestion</button>";
         echo "<div class='suggestion-form' style='display:none;'>";
-        echo "<input type='text' class='sug-options' name='terms' placeholder='Terms'>";
-        echo "<input type='text'class='sug-options'  name='amount' placeholder='Amount'>";
-        echo "<input type='text' class='sug-options' name='duration' placeholder='Duration'><br>";
-        echo "<button type='submit' class='submit-sug' name='submit'>Submit</button>";
+        echo "<input type='text' name='terms' placeholder='Terms'>";
+        echo "<input type='text' name='amount' placeholder='Amount'>";
+        echo "<input type='text' name='duration' placeholder='Duration'>";
+        echo "<button type='submit' name='submit'>Submit</button>";
         echo "<input type='hidden' name='id' value='".$row['id']."'>";
         echo "</div>";
         echo "</form>";
@@ -107,7 +107,7 @@ echo "<h2>Entreprises</h2>";
         echo "<td>";
         //add a button to go to the massage.php page
         echo "<form method='post'>";
-        echo "<button type='submit' class='message-btn' name='message'>Message</button>";
+    echo "<button type='submit' name='message' class='message-btn' id='message'>Message</button>";
         echo "<input type='hidden' name='id' value='".$row['id']."'>";
         echo "</form>";
         echo "</td>";
@@ -134,9 +134,11 @@ echo "<h2>Entreprises</h2>";
         //check if the result is true
         if($result) {
             //if true redirect to the same page
-            header("Location: influenceur.php");
+?>
+      <script type="text/javascript">window.location="influenceur.php";</script>
+<?php
             exit();
-    }else{
+}else{
         //if false show an error message
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
@@ -151,9 +153,11 @@ echo "<h2>Entreprises</h2>";
         $_SESSION['type'] = "inf";
         $_SESSION['id']= $id;
         //redirect to the message page
-        header("Location: message_inf.php");
-        exit();
-    }
+?>
+      <script type="text/javascript">window.location="message_inf.php";</script>
+<?php
+exit();
+}
 
     echo "<script>
     document.querySelectorAll('.suggestion-btn').forEach(item => {
@@ -192,14 +196,14 @@ echo "<h2>Entreprises</h2>";
             echo "<td>";
             echo "<form method='post'>";
             //add a button to accept the offer
-            echo "<button type='submit' class='accept-btn' name='accept'>Accept</button>";
+            echo "<button type='submit' name='accept'class='accept-btn' id='accept'>Accept</button>";
             echo "<input type='hidden' name='id' value='".$row['id']."'>";
             echo "</form>";
             echo "</td>";
             echo "<td>";
             echo "<form method='post'>";
             //add a button to refuse the offer WITH Adiffernt name
-            echo "<button type='submit' class='refuse-btn' name='refuse'>Refuse</button>";
+            echo "<button type='submit' name='refuse' class='refuse-btn' id='refuse'>Refuse</button>";
             echo "<input type='hidden' name='id' value='".$row['id']."'>";
             echo "</form>";
             echo "</td>";
@@ -219,9 +223,11 @@ echo "<h2>Entreprises</h2>";
         //check if the request is successful
         if($result) {
             //if successful redirect to the influencer page
-            header("Location: influenceur.php");
-            exit();
-        } else {
+?>
+      <script type="text/javascript">window.location="influenceur.php";</script>
+<?php            
+exit();
+} else {
             //if not successful show an error message
             echo "Error: " . $sql . "<br>" . mysqli_error($conn);
         }
@@ -238,9 +244,11 @@ echo "<h2>Entreprises</h2>";
         //check if the request is successful
         if($result) {
             //if successful redirect to the influencer page
-            header("Location: influenceur.php");
-            exit();
-        } else {
+?>
+      <script type="text/javascript">window.location="influenceur.php";</script>
+<?php
+exit();
+} else {
             //if not successful show an error message
             echo "Error: " . $sql . "<br>" . mysqli_error($conn);
         }
@@ -277,4 +285,5 @@ echo "<h2>Entreprises</h2>";
 ?>
 </div>
 
-
+</body>
+</html>
