3de22536824c11f66b35d6796f534d01587077e0
simo <ezzouak2001@gmail.com>
2023-05-12 16:58:22 +0100

    finish boys

--- modifypfinf.php
+++ modifypfinf.php
@@ -5,9 +5,10 @@ include 'database.php';
 
 //verifier que l'influenceur est authentifié
 if (!isset($_SESSION['id'])) {
-    //s'il n'est pas => direction vers loogin.php
-    header('Location: index.php');
-    exit;
+    //s'il n'est pas => direction vers loogin.php using js
+    echo "<script>window.location.href='login.php';</script>";
+    exit();
+
 }
 //id de l'influenceur
 $id = $_SESSION['id'];
@@ -15,6 +16,8 @@ $id = $_SESSION['id'];
 $sql = "SELECT * FROM influencer WHERE id = '$id'";
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result);
+//stocker le nom , email, age et instagram
+$logo = 'Upload/' . $row['pfp'];
 ?>
 <!DOCTYPE html>
 <html>
@@ -22,18 +25,19 @@ $row = mysqli_fetch_assoc($result);
     
         <title>Modifier profile influenceur</title>
         <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <link rel="stylesheet" type="text/css" href="./css/modifypfinf.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     </head>
     <!-- formulaire  -->
     <body>
-    <form action="modifypfinf.php" method="post"    enctype="multipart/form-data">
-             <!-- bouton cancel pour revenir à  influencer.php -->
-        <div class="clearfix">
-                    <button type="submit" class="cancelbtn" name="cancel"><i class="fa fa-arrow-left"></i></button>
-                </div>
-            <div class="container">
+    <center>
+
+    <div class="box">
+        <form action="" method="post" enctype="multipart/form-data">
+                <img src="<?php echo $logo; ?>" alt="logo">
+                <input type="file" name="pfp"  id="file" accept="image/*">
                 <h1>Modifier profil influenceur</h1>
                 <p>Remplissez ce formulaire pour modifier votre profil.</p>
                 <hr>
@@ -46,61 +50,112 @@ $row = mysqli_fetch_assoc($result);
                 <!-- valeur par defaut:email de l'influenceur-->
                 <label for="email"><b>Email</b></label>
                 <input type="text" placeholder="Entrer votre email" name="email" value="<?php echo $row['email']; ?>" required>
+                <!-- valeur par defaut :telephone -->
+                <label for="phone"><b>Téléphone</b></label>
+                <input type="text" placeholder="Entrer votre téléphone" name="phone" value="<?php echo $row['telephone']; ?>" required>
+                <!-- valeur par defaut:age de l'influenceur-->
                 <label for="age"><b>Age</b></label>
                 <!-- valeur par defaut: age de l'influenceur -->
                 <input type="text" placeholder="Entrer votre age" name="age" value="<?php echo $row['age']; ?>" required>
+                <i class="fa fa-instagram" style="font-size:24px"></i>
                 <label for="instagram"><b>Instagram</b></label>
                 <!-- valeur par defaut: lien du compte instagram de l'influenceur-->
                 <input type="text" placeholder="Entrer votre instagram" name="instagram" value="<?php echo $row['insta']; ?>">
                 <!-- valeur par defaut:lien du compte facebook de l'influenceur-->
+                <i class="fa fa-facebook-official" style="font-size:24px"></i>
                 <label for="facebook"><b>Facebook</b></label>
                 <input type="text" placeholder="Entrer votre facebook" name="facebook" value="<?php echo $row['fcbk']; ?>" >
                 <!-- valeur par defaut:lien de la chaine youtube de l'influenceur-->
+                <i class="fa fa-youtube-play" style="font-size:24px"></i>
                 <label for="youtube"><b>Youtube</b></label>
                 <input type="text" placeholder="Entrer votre youtube" name="youtube" value="<?php echo $row['youtube']; ?>" >
+                <!-- Domaine ,valeur par defaut: domaine de l'influenceur-->
+                <label for="domaine"><b>Domaine</b></label>
+                <select name="domaine" id="domaine" value="<?php echo $row['domaine'];?>" required>
+                           <option value="Fashion">Fashion</option>
+                           <option value="Sport">Sport</option>
+                           <option value="Art">Art</option>
+                           <option value="Cuisine">Cuisine</option>
+                           <option value="autre">Autre</option>
+                </select>
+                <!-- valeur par defaut:abonne de l'influenceur-->
+                <label for="abonne"><b>Abonnés</b></label>
+                <select name="follower" id="follower">
+                            <option value="30k-60k">30k-60k Abonné</option>
+                            <option value="60k-90k">60k-90k Abonné</option>
+                            <option value="90k-150k">90k-150k Abonné</option>
+                            <option value="+150k">+150k Abonné</option>
+                        </select> 
                  <label for="logo"><b>Photo de profile</b></label>
-            <input type="file" placeholder="Entrer la photo de profile" name="logo" value="<?php echo $logo; ?>">
-                <div class="clearfix">
-                    <button type="submit" class="signupbtn" name="submit">Modifier</button>
-                </div>
+                 <br>
+                <input type="file" placeholder="Entrer la photo de profile" name="logo" value="<?php echo $logo; ?>">
+                <button type="submit" class="signupbtn" name="submit" style="float: right; margin:10px 18.2% 0 0;">Enregistrer</button>
+                
+                <button type="submit" name="cancel" value="cancel" style="float: left;margin:10px 0 0 18.2%;">Annuler</button>
+                
                 
 
-            </div>
+            
         </form>
+    </div>
+        </center>
     </body>
 </html>
 
 <?php
 //si l'influenceur clique sur cancel
-if (isset($_POST['cancel'])) {
-    //redirection vers influenceur.php
-    header("Location:influenceur.php");
-    exit;
+if(isset($_POST['cancel'])){
+    //rediriger vers la page d'accueil
+    header('Location: influenceur.php');
 }
-//si l'influenceur clique sur submit
-if (isset($_POST['submit'])) {
-    //recuperer les données du formulaire:
+// si on clique sur le bouton enregistrer
+if(isset($_POST['submit'])){
+    //recuperer les valeurs du formulaire
     $lastname = $_POST['lastname'];
     $firstname = $_POST['firstname'];
     $email = $_POST['email'];
+    $phone = $_POST['phone'];
     $age = $_POST['age'];
-     $logo=$_FILES['logo']['name'];
-    $tmp_name=$_FILES['logo']['tmp_name'];
-    //modifier les anciennes informations dans la base de données
-    //deplacer le logo vers le dossier upload
-    move_uploaded_file($tmp_name, "Upload/$logo");
-    //modifier les anciennes informations dans la base de données
-    $sql = "UPDATE influencer SET nom='$lastname',prenom='$firstname',email='$email',age='$age',pdp='$logo' WHERE id='$id'";
-    $result = mysqli_query($conn, $sql);
-    //si bien modifiées
-    if ($result) {
-        //redirection vers influenceur.php
-        header('Location: influenceur.php');
-        exit;
-    } else {
-        //sinon:message d'erreur
-        echo "Error updating record: " . mysqli_error($conn);
+    $instagram = $_POST['instagram'];
+    $facebook = $_POST['facebook'];
+    $youtube = $_POST['youtube'];
+    $domaine = $_POST['domaine'];
+    // check if the user has chosen a follower range
+    if(isset($_POST['follower'])){
+        $follower = $_POST['follower'];
+    }else{
+        $follower = $row['abonne'];
     }
-}
+     //recuperer le nom du logo
+     $pfp=$_FILES['pfp']['name'];
+     //recuperer le chemin du logo
+     $target="Upload/".basename($_FILES['pfp']['name']);
+     //si on a choisi un logo
+     if($pfp){
+         //modifier le pfp
+            $sql="UPDATE influencer SET pfp='$pfp' WHERE id='$id'";
+            $result=mysqli_query($conn,$sql);
+            //deplacer le logo vers le dossier Upload
+            move_uploaded_file($_FILES['pfp']['tmp_name'],$target);
+            // update the other fields
+            $sql="UPDATE influencer SET nom='$lastname',prenom='$firstname',email='$email',telephone='$phone',age='$age',insta='$instagram',fcbk='$facebook',youtube='$youtube',domaine='$domaine',abonne='$follower' WHERE id='$id'";
+            mysqli_query($conn,$sql);
+            //rediriger vers la page d'accueil
+            ?>
+<!-- rediriger vers loogin.php -->
+      <script type="text/javascript">window.location="influenceur.php";</script>
+<?php
+     }else{
+         // update the other fields
+            $sql="UPDATE influencer SET nom='$lastname',prenom='$firstname',email='$email',telephone='$phone',age='$age',insta='$instagram',fcbk='$facebook',youtube='$youtube',domaine='$domaine',abonne='$follower' WHERE id='$id'";
+            mysqli_query($conn,$sql);
+            //rediriger vers la page d'accueil
+?>
+<!-- rediriger vers loogin.php -->
+      <script type="text/javascript">window.location="influenceur.php";</script>
+<?php
+     }
+    
+}  
 ?>
 
